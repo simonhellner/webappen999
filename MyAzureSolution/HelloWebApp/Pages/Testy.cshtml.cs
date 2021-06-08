cc213d388d8cc4599ce0b94d68ff2f295dc41308
Hellner <simon.hellner@sogeti.com>
2021-06-08 11:15:17 +0200

    first commit

---
+++ MyAzureSolution/HelloWebApp/Pages/Testy.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWebApp.Pages
+{
+    public class TestyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
